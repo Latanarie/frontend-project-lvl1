9bc6f8a608bdd13d73936c4738bba7c54ef62efa
Evgeniya Vaschenko <vaschenko.eo@gmail.com>
2022-05-12 15:56:27 +1000

    eslint check

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const meeting = () => {
+const meeting = () => {
   const userName = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${userName}!`);
 };
+
+export default meeting;
