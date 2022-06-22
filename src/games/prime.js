5d46cf6bf8ba48d8febf7c54548ce88fbfc6564f
Evgeniya Vaschenko <vaschenko.eo@gmail.com>
2022-06-22 23:22:37 +1000

    fix refactoring 3

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,7 +4,7 @@ import getRandomNumber from '../utils.js';
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no"';
 
 const isPrimeNumber = (number) => {
-  if (number === 1) return false;
+  if (number <= 1) return false;
   for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
       return false;
